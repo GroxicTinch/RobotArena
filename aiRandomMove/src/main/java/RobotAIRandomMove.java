8949d2e15fbdb0bab33450e18d3e0936272a314f
David DarkVamprism <DarkVamprism+Github@gmail.com>
2019-10-27 03:47:27 +0800

    Change native AI to build\use both 32 and 64bit library.

--- aiRandomMove/src/main/java/RobotAIRandomMove.java
+++ aiRandomMove/src/main/java/RobotAIRandomMove.java
@@ -3,7 +3,22 @@ package robotarena;
 public class RobotAIRandomMove implements RobotAI {
   static {//[FIXME] Need to make project auto build native code
     try {
-      System.loadLibrary("aiRandomMove");
+      // If the current computer is a 64bit then load the 64bit .dll else load the 32bit
+      String nativeLibPath;
+      switch (System.getProperty("sun.arch.data.model")){
+        case "64":
+          nativeLibPath = "x64";
+          break;
+        // case "32":
+        default:
+          nativeLibPath = "x86";
+          break;
+      }
+
+      nativeLibPath += "/aiRandomMove";
+
+      System.loadLibrary(nativeLibPath);
+
     } catch (UnsatisfiedLinkError e) {
       System.out.println("aiRandomMove library failed to load.\n" + e);
       System.exit(1);
